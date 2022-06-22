e2e116626280f624badbc0c62d05cf4d87a1c5bf
AbdulAzeez002 <azeeztp2020@gmail.com>
2022-06-22 18:02:42 +0530

    yy

--- routes/user.js
+++ routes/user.js
@@ -745,7 +745,7 @@ router.get('/brand/:id', (req, res) => {
 
 router.post('/search-filter', (req, res) => {
   console.log('88888888888888888888888888888888888888888888888888888888888888888');
-  console.log(req.body);
+  // console.log(req.body);
  
   let a = req.body
   let price = parseInt(a.Prize)
